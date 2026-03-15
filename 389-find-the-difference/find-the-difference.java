834f6c3a92198db7c6d98f06a4a8de1e34a3eab2
omerhan3to2to3 <128032168+omerhan3to2to3@users.noreply.github.com>
2026-03-15 15:14:48 +0300

    Time: 3 ms (39.28%) | Memory: 43.5 MB (17.29%) - LeetSync

--- 389-find-the-difference/find-the-difference.java
+++ 389-find-the-difference/find-the-difference.java
@@ -1,17 +1,13 @@
-import java.util.Arrays;
 
 class Solution {
     public char findTheDifference(String s, String t) {
-        // Stringleri char array'e çeviriyoruz
         char[] sChars = s.toCharArray();
         char[] tChars = t.toCharArray();
         
-        // Karakter dizilerini sıralıyoruz
         Arrays.sort(sChars);
         Arrays.sort(tChars);
         
         int i = 0;
-        // sChars üzerinden dönüyoruz
         while (i < sChars.length) {
             if (sChars[i] != tChars[i]) {
                 return tChars[i];
@@ -19,7 +15,6 @@ class Solution {
             i++;
         }
         
-        // Eğer döngü bittiyse, farklı olan karakter t'nin son karakteridir
         return tChars[i];
     }
 }
